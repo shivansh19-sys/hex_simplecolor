204602c379dba2a8543b915917196bebac25a0d2
shivansh19-sys <56798405+shivansh19-sys@users.noreply.github.com>
2020-10-21 23:08:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
-const idni = document.getElementById("idn");
+const idni = document.querySelector(".idn");
 
 btn.addEventListener("click", function () {
   const randomNumber = getRandomNumber();
